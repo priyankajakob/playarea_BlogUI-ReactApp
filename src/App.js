f60a59baf9be42e603f973493e24b89dd25bdbac
Priyanka Jacob <priyankajacob@Priyankas-MacBook-Air.local>
2019-08-17 23:30:20 +0530

    sdsd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props){
+    return (
+        <BrowserRouter>
+        <div>
+            <h2>Welcome To Blog UI.</h2>
+            <Link to="/"> Home</Link> ||
+            <Link to ="/posts"> Lists of Posts</Link> ||
+            <Link to ="/authors"> Lists of Authors</Link>
+
+        </div>
+        </BrowserRouter>
+    )
 }
+export default App
 
-export default App;
